5545bd9308acfa90a1f5e3ce5cf24bb71c945b67
Sean Brittingham <seanbritt4@gmail.com>
2019-10-21 12:50:13 -0400

    lil bit

--- lab-04/main.c
+++ lab-04/main.c
@@ -9,7 +9,20 @@ int main(int argc, char** argv){
     MPI_Comm_rank(world, &rank);
     MPI_Comm_size(world, &nprocs);
 
-    printf("%d %d\n", rank, nprocs);
+    if(rank==0){
+        FILE* src = fopen("shadow", "r");
+        if(src == NULL){
+            printf("Error: unable to open file\n");
+            exit(1);
+        }
+
+        char b;
+        size_t nread;
+        while(nread=fread(b, 1, sizeof b, src)) > 0){
+            fwrite(b, 1, nread, stdout);
+        }
+    }
+
 
     MPI_Finalize();
     return 0;
